56ad702bdad81a5bbc6e27b8a2cba89cd921b620
miholeus <me@miholeus.com>
2018-01-28 01:39:57 +0300

    added ini files to travis build :rocket:

--- travis-build.sh
+++ travis-build.sh
@@ -14,6 +14,9 @@ if [ -n "${PHP_VERSION}" ]; then
     cp .env.dist .env
     sed -i -- "s/PHP_VERSION=.*/PHP_VERSION=${PHP_VERSION}/g" .env
     sed -i -- 's/=false/=true/g' .env
+### SET PHP INI FILES
+    sed -i -- "s/AEROSPIKE_INI_FILE=.*/AEROSPIKE_INI_FILE=aerospike.ini/g" .env
+    sed -i -- "s/XDEBUG_INI_FILE=.*/XDEBUG_INI_FILE=xdebug.ini/g" .env
     cat .env
     docker-compose build ${BUILD_SERVICE}
     docker images
